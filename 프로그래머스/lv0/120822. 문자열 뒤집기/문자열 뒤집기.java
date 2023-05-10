31b2b8cec80ad56f7e3f4ed3ff35dc8745667a82
klettermi <95194606+klettermi@users.noreply.github.com>
2023-05-10 17:10:45 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.28 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] arr = my_string.split("");
+        
+        for(int i = arr.length-1; i >= 0; i--)
+            answer += arr[i];
+        return answer;
+    }
+}
