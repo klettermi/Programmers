0fc6cb96169d6dbe3303caacffe6caeb7477f393
klettermi <95194606+klettermi@users.noreply.github.com>
2023-05-12 16:05:37 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.41 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,9 +1,6 @@
-import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        Arrays.sort(array);
-        answer = array[array.length/2];
         return answer;
     }
 }
