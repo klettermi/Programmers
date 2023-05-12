9cb8f7f354af9a22e8617d6d834e855c98140550
klettermi <95194606+klettermi@users.noreply.github.com>
2023-05-12 15:58:37 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        for(int i = 0; i < numbers.length; i++)
-            answer[i] = numbers[i] * 2;
+        int[] answer = {};
         return answer;
     }
 }
