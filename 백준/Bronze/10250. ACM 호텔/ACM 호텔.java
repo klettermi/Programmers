d8513e49592a3cd7ad2ac23e141361bac43d2d3f
klettermi <95194606+klettermi@users.noreply.github.com>
2023-05-22 20:07:56 +0900

    [Bronze III] Title: ACM 호텔, Time: 240 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for(int i  = 0; i< T; i++) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+            int x = (N/H) + 1;
+            int y = N%H;
+            if(N%H==0) {
+                x = N/H;
+                y = H;
+            }
+            System.out.println(y*100+x);
+        }
+    }
+}
