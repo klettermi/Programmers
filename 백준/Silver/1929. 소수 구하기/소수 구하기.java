2a9f49c47a11844fd3a57f755faf47f3dc09479d
klettermi <95194606+klettermi@users.noreply.github.com>
2023-05-23 11:33:48 +0900

    [Silver III] Title: 소수 구하기, Time: 952 ms, Memory: 32808 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static boolean[] prime = new boolean[1000001];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        get_prime();
+
+   
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+   
+        for(int i = m; i <= n; i++)
+            if(!prime[i]) System.out.println(i);
+        
+    }
+
+    public static void get_prime(){
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++){
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i)
+                prime[j] = true;
+        }
+    }
+}
