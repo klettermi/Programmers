21f071d9a357b154d597b8099d7b3f7851f21960
klettermi <95194606+klettermi@users.noreply.github.com>
2023-05-25 17:32:06 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] coin = new int[n];
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++)
+            coin[i] = Integer.parseInt(br.readLine());
+
+        for(int i = n-1; i >= 0; i--){
+            cnt += (k / coin[i]);
+            k = k % coin[i];
+
+            if(k==0)
+                break;
+        }
+
+        System.out.println(cnt);
+    }
+}
