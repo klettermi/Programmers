f6e601890d34bcb9b3e659b945a2fb941c70d769
klettermi <95194606+klettermi@users.noreply.github.com>
2023-06-05 13:36:02 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 532 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int fib(int n){
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+        else{
+            return(fib(n - 1) + fib(n - 2));
+        }
+    }
+
+    public static int fibonacci(int n){
+        int[] f = new int[n + 1];
+        int cnt = 0;
+        f[1] = f[2] = 1;
+        for(int i = 3; i < n + 1; i++){
+            cnt++;
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fib(n) + " " + fibonacci(n));
+    }
+}
