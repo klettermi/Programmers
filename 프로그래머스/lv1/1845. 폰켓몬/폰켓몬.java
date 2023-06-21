469e486265557aa7600ff588a3c1e26e0d176fd8
klettermi <95194606+klettermi@users.noreply.github.com>
2023-06-21 19:32:32 +0900

    [level 1] Title: 폰켓몬, Time: 2.13 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,26 +1,12 @@
-import java.util.HashMap;
 import java.util.HashSet;
 class Solution {
-    public static HashMap<Integer, Integer> makeHash(int[] nums){
-        HashMap<Integer, Integer> pocket = new HashMap<>();
+    public static int solution(int[] nums) {
+        int answer = 0;
         HashSet <Integer> set = new HashSet<>();
         for(int i : nums){
             set.add(i);
         }
-        for(int i : set){
-            int cnt = 0;
-            for(int j : nums){
-                if(i == j)
-                    cnt++;
-            }
-            pocket.put(i, cnt);
-        }
-        return pocket;
-    }
-    public static int solution(int[] nums) {
-        int answer = 0;
-        HashMap <Integer, Integer> pocket = makeHash(nums);
-        if(pocket.size() <= nums.length/2) answer = pocket.size();
+        if(set.size() <= nums.length/2) answer = set.size();
         else answer = nums.length/2;
         return answer;
     }
