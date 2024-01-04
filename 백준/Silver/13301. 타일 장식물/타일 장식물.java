8a5bf69a2c1638779498e1aa324523396d687c96
klettermi <95194606+klettermi@users.noreply.github.com>
2024-01-04 11:50:00 +0900

    [Silver V] Title: 타일 장식물, Time: 124 ms, Memory: 14208 KB, Score: 74 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        if(n == 1) answer = 4;
+        else if(n == 2) answer = 6;
+        else {
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            answer = 2 * (dp[n-1] + dp[n]);
+        }
+        System.out.println(answer);
+    }
+}
