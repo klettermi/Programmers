890011cae282951157e2a58e16fc0b3a0cce872d
klettermi <95194606+klettermi@users.noreply.github.com>
2024-01-15 11:44:05 +0900

    [Gold V] Title: 탑, Time: 780 ms, Memory: 104116 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -4,37 +4,44 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+class Top{
+    int height;
+    int index;
+
+    public Top(int height, int index){
+        this.height = height;
+        this.index = index;
+    }
+}
 public class Main {
     public static void main(String[] args) throws IOException {
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> index = new Stack<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int size = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= size; i++){
-            int num = Integer.parseInt(st.nextToken());
-            while(true){
-                if(!stack.empty()){
-                    int top = stack.peek();
-                    if(top > num){
-                        sb.append(index.peek()).append(" ");
-                        stack.push(num);
-                        index.push(i);
+
+        Stack<Top> tops = new Stack<>();
+
+        for(int i = 1; i <= n; i++){
+            int cur = Integer.parseInt(st.nextToken());
+            if(!tops.isEmpty()){
+                while(!tops.isEmpty()){
+                    Top top = tops.peek();
+                    if(top.height > cur){
+                        sb.append(top.index).append(" ");
+                        tops.push(new Top(cur, i));
                         break;
                     }else{
-                        stack.pop();
-                        index.pop();
+                        tops.pop();
                     }
-                }else{
-                    sb.append("0 ");
-                    stack.push(num);
-                    index.push(i);
-                    break;
                 }
+                if(tops.isEmpty()) sb.append("0 ");
+                tops.push(new Top(cur, i));
+            }else{
+                tops.push(new Top(cur, i));
+                sb.append("0 ");
             }
         }
         System.out.println(sb);
